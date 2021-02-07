dccfe9cc6e1230e0adab2d94b703ab477f50d4be
Lahiru Sandaruwan <lahirus250@gmail.com>
2021-02-07 16:51:31 +0530

    first commit

--- Vid6Part2ConfigConsumer/src/main/java/com/example/configconsumer/controller/ProfileController.java
+++ Vid6Part2ConfigConsumer/src/main/java/com/example/configconsumer/controller/ProfileController.java
@@ -1,2 +1,36 @@
-package com.example.configconsumer.controller;public class ProfileController {
+package com.example.configconsumer.controller;
+
+import com.example.configconsumer.model.CustomerProfileConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RestController
+//public class ProfileController {
+//
+//    @Autowired
+//    CustomerProfileConfiguration customerProfileConfiguration;
+//
+//    @RequestMapping(value = "/profile")
+//    public CustomerProfileConfiguration getConfig(){
+//        return customerProfileConfiguration;
+//    }
+//
+//}
+
+@Controller
+public class ProfileController {
+
+    @Autowired
+    CustomerProfileConfiguration customerProfileConfiguration;
+
+    @RequestMapping(value = "/profile")
+    public String getConfig(Model model){
+        model.addAttribute("item", customerProfileConfiguration.getDefaultItem());
+        model.addAttribute("min", customerProfileConfiguration.getMinBuy());
+        return "cProfile";
+    }
+
 }
--- Vid6Part2ConfigConsumer/src/main/java/com/example/configconsumer/model/CustomerProfileConfiguration.java
+++ Vid6Part2ConfigConsumer/src/main/java/com/example/configconsumer/model/CustomerProfileConfiguration.java
@@ -1,2 +1,21 @@
-package com.example.configconsumer.model;public class CustomerProfileConfiguration {
+package com.example.configconsumer.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomerProfileConfiguration {
+
+    @Autowired
+    Environment environment;
+
+    public String getDefaultItem(){
+        return environment.getProperty("product.default.item");
+    }
+
+    public String getMinBuy(){
+        return environment.getProperty("customer.buy.min");
+    }
+
 }
