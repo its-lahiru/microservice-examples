3b825c65cd280e5280ece7d7a09c5ec614f05072
Lahiru Sandaruwan <lahirus250@gmail.com>
2021-02-08 15:18:06 +0530

    initial commit

---
+++ Vid8BasicOAuthServer/src/test/java/com/lahiru/training/basicoauthserver/BasicOauthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lahiru.training.basicoauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicOauthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
